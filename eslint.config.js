ac6f472cccb37dc8d277115c8a074b7aaf3b0c71
Bin <962920136@qq.com>
2025-01-08 22:21:56 +0800

    chore(good-fn): 重构

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {},
+  {
+    files: ['packages/**/__use__/*.ts'],
+    rules: {
+      'no-alert': 'off',
+      'no-console': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'no-restricted-imports': 'off',
+      'vue/no-unused-vars': 'off',
+      'vue/no-unused-refs': 'off',
+      'vue/require-v-for-key': 'off',
+      'ts/no-unused-vars': 'off',
+      'ts/no-redeclare': 'off',
+      'unused-imports/no-unused-vars': 'off',
+    },
+  },
+)
--- package/lib/CreateCancelableTask/__use__/index.js
+++
@@ -1,7 +0,0 @@
-import { createCancelableTask } from '../index.js';
-const fetchData = (url) => {
-    return new Promise((resolve) => setTimeout(() => resolve('data from ' + url), 1000));
-};
-const { execute, cancel } = createCancelableTask(fetchData);
-execute('http://example.com/api/data1').then(res => console.log(res));
-execute('http://example.com/api/data1').then(res => console.log(res));
